d5be2d50e28daa5d33dbb600dba13ac40563b726
Martyn Clark <mclark@ucar.edu>
2013-07-08 15:00:22 +0000

    major changes to the code to handle the excessive storage issue (still testing)

--- build/source/engine/coupled_em.f90
+++ build/source/engine/coupled_em.f90
@@ -489,8 +489,8 @@ contains
  iLayer = nSnow+1
  !print*, 'nsub, mLayerTemp(iLayer), mLayerVolFracIce(iLayer) = ', nsub, mLayerTemp(iLayer), mLayerVolFracIce(iLayer)
  print*, 'nsub = ', nsub
- if(nsub>1000)then
-  message=trim(message)//'number of sub-steps > 1000'
+ if(nsub>10000)then
+  message=trim(message)//'number of sub-steps > 10000'
   err=20; return
  endif
 
