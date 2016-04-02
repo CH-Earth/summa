5b3ca51d8b54f772e1f97c6c5e9160f352a51553
luckymichael <michael.ou@hotmail.com>
2016-04-02 01:21:04 -0700

    manually estimate the elapsed time2

--- build/source/driver/multi_driver.f90
+++ build/source/driver/multi_driver.f90
@@ -1033,13 +1033,13 @@ contains
  end if
  
  ! print initial and final date and time
- write(outunit,"(A,I4,'-',I2.2,'-',I2.2,'/',I2,':',I2.2,':',I2.2,'.',I3.3)"),'initial date/time = ',ctime1(1:3),ctime1(5:8)
- write(outunit,"(A,I4,'-',I2.2,'-',I2.2,'/',I2,':',I2.2,':',I2.2,'.',I3.3)"),'  final date/time = ',ctime2(1:3),ctime2(5:8)
+ write(outunit,"(A,I4,'-',I2.2,'-',I2.2,2x,I2,':',I2.2,':',I2.2,'.',I3.3)"),'initial date/time = ',ctime1(1:3),ctime1(5:8)
+ write(outunit,"(A,I4,'-',I2.2,'-',I2.2,2x,I2,':',I2.2,':',I2.2,'.',I3.3)"),'  final date/time = ',ctime2(1:3),ctime2(5:8)
  ! print elapsed time
- write(outunit,"(/,A,1PG15.7,A)"),                                           '     elapsed time = ', elpSec,          ' s'
- write(outunit,"(A,1PG15.7,A)"),                                             '       or           ', elpSec/60_sp,    ' m'
- write(outunit,"(A,1PG15.7,A)"),                                             '       or           ', elpSec/3600_sp,  ' h'
- write(outunit,"(A,1PG15.7,A/)"),                                            '       or           ', elpSec/86400_sp, ' d'
+ write(outunit,"(/,A,1PG15.7,A)"),                                          '     elapsed time = ', elpSec,          ' s'
+ write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elpSec/60_sp,    ' m'
+ write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elpSec/3600_sp,  ' h'
+ write(outunit,"(A,1PG15.7,A/)"),                                           '       or           ', elpSec/86400_sp, ' d'
  ! stop with message
  print*,'FORTRAN STOP: '//trim(message)
  stop
