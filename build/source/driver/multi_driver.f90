b147b123e2f3840fbc621729778b24a64aba9fbf
Michael Ou <ougengxin@gmail.com>
2016-10-19 16:21:47 -0700

    remove hard-coded "spinup" in the file name of the first output file

--- build/source/driver/multi_driver.f90
+++ build/source/driver/multi_driver.f90
@@ -662,7 +662,9 @@ select case (iRunMode)
  case(iRunModeHRU)
   write(output_fileSuffix((len_trim(output_fileSuffix)+1):len(output_fileSuffix)),"('_H',i0)") checkHRU
 end select
-fileout = trim(OUTPUT_PATH)//trim(OUTPUT_PREFIX)//'spinup'//trim(output_fileSuffix)
+write(fileout,'(a,i0,a,i0,a)') trim(OUTPUT_PATH)//trim(OUTPUT_PREFIX), &
+                               timeStruct%var(iLookTIME%iyyy),'-',timeStruct%var(iLookTIME%iyyy)+1, &
+                               trim(output_fileSuffix)
 call def_output(nHRU,gru_struc(1)%hruInfo(1)%nSoil,fileout,err,message); call handle_err(err,message)
  
 ! write local model attributes and parameters to the model output file
