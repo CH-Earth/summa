9e25e51bdad0402076e0853cf1c1f65883234b5d
Reza Zolfaghari <zolfaghr@mcmaster.ca>
2021-03-14 00:24:37 -0600

    minor edit

--- build/source/engine/fidaSolver.f90
+++ build/source/engine/fidaSolver.f90
@@ -611,7 +611,7 @@ implicit none
 	integer(i4b),intent(out)            	:: retval				! return value
 
 	real(qp),parameter     					:: coef_nonlin = 0.33	! Coeff. in the nonlinear convergence test, default = 0.33
-	integer,parameter 	   					:: max_order = 5		! maximum BDF order,  default = 5
+	integer,parameter 	   					:: max_order = 1		! maximum BDF order,  default = 5
 	integer,parameter 	   					:: nonlin_iter = 100	! maximun number of nonliear iterations, default = 4	
 	integer,parameter 	   					:: acurtest_fail = 50	! maximum number of error test failures, default = 10
 	integer,parameter 	   					:: convtest_fail = 50	! maximum number of convergence test failures, default = 10
