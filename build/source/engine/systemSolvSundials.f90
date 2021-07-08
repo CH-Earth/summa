07686abb4783da8befc0dd327f3b15ca2e8f9efb
Reza Zolfaghari <zolfaghr@mcmaster.ca>
2021-07-08 10:03:20 -0600

    computing H_T for the first step in systemSolvSundials

--- build/source/engine/systemSolvSundials.f90
+++ build/source/engine/systemSolvSundials.f90
@@ -136,7 +136,7 @@ contains
  USE convE2Temp_module,only:temp2ethpy                ! convert temperature to enthalpy
  USE tol4IDA_module,only:popTol4IDA                   ! pop tolerances 
  USE solveByIDA_module,only:solveByIDA                ! solve DAE by IDA
-! use varExtrSundials_module, only:countDiscontinuity
+ USE t2enthalpy_module, only:t2enthalpy_T             ! compute enthalpy
  use, intrinsic :: iso_c_binding
  implicit none
  ! ---------------------------------------------------------------------------------------
@@ -219,10 +219,21 @@ contains
  ! model decisions
  ixGroundwater           => model_decisions(iLookDECISIONS%groundwatr)%iDecision   ,& ! intent(in):    [i4b]    groundwater parameterization
  ixSpatialGroundwater    => model_decisions(iLookDECISIONS%spatial_gw)%iDecision   ,& ! intent(in):    [i4b]    spatial representation of groundwater (local-column or single-basin)
- ! check the need to merge snow layers
- mLayerTemp              => prog_data%var(iLookPROG%mLayerTemp)%dat                ,& ! intent(in):    [dp(:)]  temperature of each snow/soil layer (K)
- mLayerVolFracLiq        => prog_data%var(iLookPROG%mLayerVolFracLiq)%dat          ,& ! intent(in):    [dp(:)]  volumetric fraction of liquid water (-)
- mLayerVolFracIce        => prog_data%var(iLookPROG%mLayerVolFracIce)%dat          ,& ! intent(in):    [dp(:)]  volumetric fraction of ice (-)
+  ! enthalpy
+ scalarCanairEnthalpy    => diag_data%var(iLookDIAG%scalarCanairEnthalpy)%dat(1)   ,&  ! intent(out): [dp]    enthalpy of the canopy air space (J m-3)
+ scalarCanopyEnthalpy    => diag_data%var(iLookDIAG%scalarCanopyEnthalpy)%dat(1)   ,&  ! intent(out): [dp]    enthalpy of the vegetation canopy (J m-3)
+ mLayerEnthalpy          => diag_data%var(iLookDIAG%mLayerEnthalpy)%dat            ,&  ! intent(out): [dp(:)] enthalpy of the snow+soil layers (J m-3)
+ ! model state variables
+ scalarCanairTemp        => prog_data%var(iLookPROG%scalarCanairTemp)%dat(1)       ,& ! intent(in): [dp]     temperature of the canopy air space (K)
+ scalarCanopyTemp        => prog_data%var(iLookPROG%scalarCanopyTemp)%dat(1)       ,& ! intent(in): [dp]     temperature of the vegetation canopy (K)
+ scalarCanopyIce         => prog_data%var(iLookPROG%scalarCanopyIce)%dat(1)        ,& ! intent(in): [dp]     mass of ice on the vegetation canopy (kg m-2)
+ scalarCanopyWat         => prog_data%var(iLookPROG%scalarCanopyWat)%dat(1)        ,& ! intent(in): [dp]     mass of total water on the vegetation canopy (kg m-2)
+ ! model state variables (snow and soil domains)
+ mLayerTemp              => prog_data%var(iLookPROG%mLayerTemp)%dat                ,& ! intent(in): [dp(:)]  temperature of each snow/soil layer (K)
+ mLayerVolFracIce        => prog_data%var(iLookPROG%mLayerVolFracIce)%dat          ,& ! intent(in): [dp(:)]  volumetric fraction of ice (-)
+ mLayerVolFracLiq        => prog_data%var(iLookPROG%mLayerVolFracLiq)%dat          ,& ! intent(in): [dp(:)]  volumetric fraction of liquid water (-)
+ mLayerVolFracWat        => prog_data%var(iLookPROG%mLayerVolFracWat)%dat          ,& ! intent(in): [dp(:)]  volumetric fraction of total water (-)
+ mLayerMatricHead        => prog_data%var(iLookPROG%mLayerMatricHead)%dat          ,& ! intent(inout): [dp(:)]  matric head (m)
  mLayerDepth             => prog_data%var(iLookPROG%mLayerDepth)%dat               ,& ! intent(in):    [dp(:)]  depth of each layer in the snow-soil sub-domain (m)
  snowfrz_scale           => mpar_data%var(iLookPARAM%snowfrz_scale)%dat(1)         ,& ! intent(in):    [dp]     scaling parameter for the snow freezing curve (K-1)
  airtemp                 => forc_data%var(iLookFORCE%airtemp)                      ,& ! intent(in):    [dp]     temperature of the upper boundary of the snow and soil domains (K)
@@ -318,6 +329,32 @@ contains
  ! try to accelerate solution for energy
  if(ixCasNrg/=integerMissing) stateVecTrial(ixCasNrg) = stateVecInit(ixCasNrg) + (airtemp - stateVecInit(ixCasNrg))*tempAccelerate
  if(ixVegNrg/=integerMissing) stateVecTrial(ixVegNrg) = stateVecInit(ixVegNrg) + (airtemp - stateVecInit(ixVegNrg))*tempAccelerate
+
+
+ ! compute H_T at the beginning of the data step
+ call t2enthalpy_T(&
+                ! input: data structures
+                diag_data,                   & ! intent(in):  model diagnostic variables for a local HRU
+                mpar_data,                   & ! intent(in):  parameter data structure
+                indx_data,                   & ! intent(in):  model indices
+                lookup_data,                 & ! intent(in):  lookup table data structure
+                ! input: state variables for the vegetation canopy
+                scalarCanairTemp,            & ! intent(in):  value of canopy air temperature (K)
+                scalarCanopyTemp,            & ! intent(in):  value of canopy temperature (K)
+                scalarCanopyWat,             & ! intent(in):  value of canopy total water (kg m-2)
+                scalarCanopyIce,             & ! intent(in):  value for canopy ice content (kg m-2)
+                ! input: variables for the snow-soil domain
+                mLayerTemp,                  & ! intent(in):  vector of layer temperature (K)
+                mLayerVolFracWat,            & ! intent(in):  vector of volumetric total water content (-)
+                mLayerMatricHead,            & ! intent(in):  vector of total water matric potential (m)
+                mLayerVolFracIce,            & ! intent(in):  vector of volumetric fraction of ice (-)
+                ! output: enthalpy
+                scalarCanairEnthalpy,        & ! intent(out): temperature component of enthalpy of the canopy air space (J m-3)
+                scalarCanopyEnthalpy,        & ! intent(out): temperature component of enthalpy of the vegetation canopy (J m-3)
+                mLayerEnthalpy,              & ! intent(out): temperature component of enthalpy of each snow+soil layer (J m-3)
+                ! output: error control
+                err,cmessage)                  ! intent(out): error control
+if(err/=0)then; message=trim(message)//trim(cmessage); return; endif
  
  ! compute the flux and the residual vector for a given state vector
  ! NOTE 1: The derivatives computed in eval8summa are used to calculate the Jacobian matrix for the first iteration
