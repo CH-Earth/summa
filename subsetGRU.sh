c7384999c9155e2148624fbeefa14d056ad3728d
ashleymedin <ashley.vanbeusekom@gmail.com>
2022-04-05 10:47:24 +0900

    the HRU id was wrong

--- subsetGRU.sh
+++ subsetGRU.sh
@@ -10,45 +10,55 @@ module load nco/5.0.6
 
 # GRU want to subset, change to do another GRU
 # first basin in the slurm batch run, 20th script
-nbasin_slurm = 1
-nscript_slurm = 20
-GRU_id =$((154 + nbasin_slurm + 207*nscript_slurm))
-HRU_id =$((154 + nbasin_slurm + 207*nscript_slurm))
+# could also just set this as id from G* on output nc files (here 4295)
+# However, this is not the actual GRUid, it is one off from the nc files, so subtract one before ncks
+nbasin_slurm=1
+nscript_slurm=20
+GRU_file=$((154 + nbasin_slurm + 207*nscript_slurm))
+GRU_id=$((GRU_file - 1))
+HRU_id=$GRU_id
+echo "file name id is 
 
 # top paths, could change
-homePath = /globalhome/gwu479/HPC/TestScripts/
-summa_exe= /globalhome//gwu479/HPC/SummaSundials/summa/bin/summa_sundials.exe
-forcingPath  = /project/gwf/gwf_cmt/rez299/summaWorkflow_data/domain_NorthAmerica/forcing/4_SUMMA_input/
+homePath=/globalhome/gwu479/HPC/TestScripts/
+summa_exe=/globalhome/gwu479/HPC/SummaSundials/summa/bin/summa_sundials.exe
+forcingPath=/project/gwf/gwf_cmt/rez299/summaWorkflow_data/domain_NorthAmerica/forcing/4_SUMMA_input/
 
 # in paths, probably won't change
-fileManager_in  = ${homePath}fileManager.txt
-settingsPath = ${homePath}settings/
-initConditionFile_in = ${settingsPath}coldState.nc
-attributeFile_in = ${settingsPath}attributes.nc
-trialParamFile_in = ${settingsPath}trialParams.nc
+fileManager_in=${homePath}fileManager.txt
+settingsPath=${homePath}settings/
+initConditionFile_in=${settingsPath}coldState.nc
+attributeFile_in=${settingsPath}attributes.nc
+trialParamFile_in=${settingsPath}trialParams.nc
 
 # out paths, probably won't change
-fileManager_out  = ${homePath}fileManager_${GRU_id}.txt
-initConditionFile_out = ${settingsPath}coldState_${GRU_id}.nc
-attributeFile_out = ${settingsPath}attributes_${GRU_id}.nc
-trialParamFile_out = ${settingsPath}trialParams_${GRU_id}.nc
-desForcingPath = ${homePath}forcing_${GRU_id})/
+fileManager_out=${homePath}fileManager_${GRU_file}.txt
+initConditionFile_out=${settingsPath}coldState_${GRU_file}.nc
+attributeFile_out=${settingsPath}attributes_${GRU_file}.nc
+trialParamFile_out=${settingsPath}trialParams_${GRU_file}.nc
+desForcingPath=/project/gwf/gwf_cmt/gwu479/summaWorkflow_data/domain_NorthAmerica/forcing/4_SUMMA_input_${GRU_file}/
 
 # set up directory and new file Manager (will have to change things in it manually as above)
-mkdir $desForcingPath
+mkdir -p "$desForcingPath"
 cp $fileManager_in $fileManager_out
 
 # do the subset
-ncks -d gru $GRU_id,$GRU_id -d hru $HRU_id,$HRU_id $initConditionFile_in $initConditionFile_out
-ncks -d gru $GRU_id,$GRU_id -d hru $HRU_id,$HRU_id $attributeFile_in $attributeFile_out
-ncks -d gru $GRU_id,$GRU_id -d hru $HRU_id,$HRU_id $trialParamFile_in $trialParamFile_out
+ncks -d hru,$HRU_id,$HRU_id $initConditionFile_in $initConditionFile_out
+echo "coldState.nc HRU ${HRU_id} subsetted"
+ncks -d gru,$GRU_id,$GRU_id -d hru,$HRU_id,$HRU_id $attributeFile_in $attributeFile_out
+echo "attributes.nc GRU ${GRU_id} HRU ${HRU_id} subsetted"
+ncks -d hru,$HRU_id,$HRU_id $trialParamFile_in $trialParamFile_out
+echo "trialParams.nc HRU ${HRU_id} subsetted"
 
 # forcing subset has multiple files
-for fn in ${forcingPath}*.nc; do
-    output_fn = ${desForcingPath}${fn}
-    output = ncks -d gru $GRU_id,$GRU_id -d hru $HRU_id,$HRU_id $fn $output_fn
+cd $forcingPath
+for fn in *.nc; do
+    output_fn=${desForcingPath}${fn}
+    ncks -d hru,$HRU_id,$HRU_id $fn $output_fn
+    echo "${fn} HRU ${HRU_id} subsetted"
+done
+cd $homePath
 
 # write summa command call file
-runFile = ${homePath}run_${GRU_id}.sh
-summaCommand = $summa_exe -p never -s _testSumma -m $fileManger_out -r e
-$summaCommand > runFile
+runFile=${homePath}run_${GRU_file}.sh
+echo "${summa_exe} -p never -s _testSumma -m ${fileManager_out} -r e" > $runFile
