d397ac02055189cfe770fedd12dee7a4ad821861
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-02-17 22:42:21 +0900

    turn off special flux update for canopy and add back in to only update top layer transpiration in comments ... leave off for now

--- build/source/engine/varSubstep.f90
+++ build/source/engine/varSubstep.f90
@@ -551,7 +551,15 @@ subroutine varSubstep(&
             ixMax=ubound(flux_data%var(iVar)%dat)
             do ixLayer=ixMin(1),ixMax(1)
               if(fluxMask%var(iVar)%dat(ixLayer)) then
-                flux_data%var(iVar)%dat(ixLayer) = flux_data%var(iVar)%dat(ixLayer) + flux_temp%var(iVar)%dat(ixLayer)*dt_wght
+
+                ! Makes Jacobian more diagonal if do this, but less correct
+                ! special case of the transpiration sink from soil layers: only computed for the top soil layer
+                !if(iVar==iLookFlux%mLayerTranspire)then
+                !  if(ixLayer==1)  flux_data%var(iVar)%dat(:) = flux_data%var(iVar)%dat(:) + flux_temp%var(iVar)%dat(:)*dt_wght
+                ! standard case
+                !else
+                  flux_data%var(iVar)%dat(ixLayer) = flux_data%var(iVar)%dat(ixLayer) + flux_temp%var(iVar)%dat(ixLayer)*dt_wght
+                !endif
                 fluxCount%var(iVar)%dat(ixLayer) = fluxCount%var(iVar)%dat(ixLayer) + 1
               endif
             end do
