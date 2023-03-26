221a61a5e6a5c01f370a7ef3a142025f5fecab21
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-03-27 00:43:40 +0900

    add register_bmi for ngen

--- build/source/driver/summa_driver.f90
+++ build/source/driver/summa_driver.f90
@@ -993,6 +993,35 @@ module summa_driver
      end select
    end function summa_set_at_indices_double
 
+#ifdef NGEN_ACTIVE
+  function register_bmi(this) result(bmi_status) bind(C, name="register_bmi")
+   use, intrinsic:: iso_c_binding, only: c_ptr, c_loc, c_int
+   use iso_c_bmif_2_0
+   implicit none
+   type(c_ptr) :: this ! If not value, then from the C perspective `this` is a void**
+   integer(kind=c_int) :: bmi_status
+   !Create the model instance to use
+   type(summa_bmi), pointer :: bmi_model
+   !Create a simple pointer wrapper
+   type(box), pointer :: bmi_box
+
+   !allocate model
+   allocate(summa_bmi::bmi_model)
+   !allocate the pointer box
+   allocate(bmi_box)
+
+   !associate the wrapper pointer the created model instance
+   bmi_box%ptr => bmi_model
+
+   if( .not. associated( bmi_box ) .or. .not. associated( bmi_box%ptr ) ) then
+    bmi_status = BMI_FAILURE
+   else
+    !Return the pointer to box
+    this = c_loc(bmi_box)
+    bmi_status = BMI_SUCCESS
+ end function register_bmi
+#endif
+
    ! non-BMI helper function to get fields, only get first do_nHRU of them
    subroutine get_basin_field(this, name, do_nHRU, targetarr)
      implicit none
