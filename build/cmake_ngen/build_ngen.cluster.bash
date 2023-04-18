f1366f81e606b14b95cc338491da766b8c473cd3
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-04-18 20:01:35 +0900

    cmake works on mac now

--- build/cmake_ngen/build_ngen.cluster.bash
+++ build/cmake_ngen/build_ngen.cluster.bash
@@ -31,7 +31,7 @@ cmake --build extern/snow17/cmake_build --target all
 cmake -B extern/summa/cmake_build -S extern/summa
 cmake --build extern/summa/cmake_build --target all
 
-cmake -B /ngen/cmake_build -S . -DMPI_ACTIVE:BOOL=OFF -DMPI_ACTIVE:BOOL=ON -DNGEN_ACTIVATE_PYTHON:BOOL=OFF -DBMI_C_LIB_ACTIVE:BOOL=ON -DBMI_FORTRAN_ACTIVE:BOOL=ON
+cmake -B cmake_build -S . -DMPI_ACTIVE:BOOL=OFF -DNGEN_ACTIVATE_PYTHON:BOOL=OFF -DBMI_C_LIB_ACTIVE:BOOL=ON -DBMI_FORTRAN_ACTIVE:BOOL=ON
 # can also add -DCMAKE_BUILD_TYPE=Debug to be able to run in gdb
 # make -j 8 -C cmake_build    # build w/ 8 parallel jobs, also turn MPI on
 make -C cmake_build
--- build/cmake_ngen/build_ngen.mac.bash
+++ build/cmake_ngen/build_ngen.mac.bash
@@ -8,10 +8,10 @@ cmake --build extern/iso_c_fortran_bmi/cmake_build --target all
 cmake -B extern/noah-owp-modular/cmake_build -S extern/noah-owp-modular
 cmake --build extern/noah-owp-modular/cmake_build --target all
 
-cmake -B extern/summa/cmake_build -S extern/summa
+cmake -B extern/summa/cmake_build -S extern/summa -DCMAKE_BUILD_TYPE=Release
 cmake --build extern/summa/cmake_build --target all
 
-cmake -B /ngen/cmake_build -S . -DMPI_ACTIVE:BOOL=OFF -DMPI_ACTIVE:BOOL=ON -DNGEN_ACTIVATE_PYTHON:BOOL=OFF -DBMI_C_LIB_ACTIVE:BOOL=ON -DBMI_FORTRAN_ACTIVE:BOOL=ON
+cmake -B cmake_build -S . -DMPI_ACTIVE:BOOL=OFF -DNGEN_ACTIVATE_PYTHON:BOOL=OFF -DBMI_C_LIB_ACTIVE:BOOL=ON -DBMI_FORTRAN_ACTIVE:BOOL=ON
 # can also add -DCMAKE_BUILD_TYPE=Debug to be able to run in gdb
 # make -j 8 -C cmake_build    # build w/ 8 parallel jobs, also turn MPI on
 make -C cmake_build
