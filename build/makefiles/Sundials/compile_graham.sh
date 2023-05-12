8b34561c6ea296f4a407f0653e71214a968c564b
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-05-12 12:59:07 +0900

    more makefiles

---
+++ build/makefiles/Sundials/compile_graham.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#### load modules if using Compute Canada or Copernicus ####
+module load StdEnv/2020
+module load gcc/9.3.0
+module load openblas/0.3.17
+module load netcdf-fortran/4.5.2
+
+#### parent directory of the 'build' directory ####
+export ROOT_DIR=/home/avanb/SummaSundials
+
+export INCLUDES="-I${EBROOTNETCDFMINFORTRAN}/include"
+export LDFLAGS="-L${EBROOTNETCDFMINFORTRAN}/lib64 -L${EBROOTOPENBLAS}/lib"
+export LIBRARIES="${LDFLAGS} -lnetcdff -lopenblas"
+
+make -f ${ROOT_DIR}/summa/build/makefiles/makefile_cluster
+export LD_LIBRARY_PATH=${ROOT_DIR}/summa/bin
---
+++ build/makefiles/Sundials/compile_mac.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#### parent directory of the 'build' directory ####
+export ROOT_DIR=/Users/amedin/Research/SummaSundials
+
+export INCLUDES="-I/opt/local/include -I/opt/local/lib"
+export LDFLAGS="-L/opt/local/lib"
+export LIBRARIES="${LDFLAGS} -llapack -lgfortran -lnetcdff -lnetcdf"
+
+make -f ${ROOT_DIR}/summa/build/makefiles/makefile_mac
+export LD_LIBRARY_PATH=${ROOT_DIR}/summa/bin
