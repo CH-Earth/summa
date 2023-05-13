6348fc8b9839a2bab59f9e0a966c3d69aadbb527
seantrim <strim201325@gmail.com>
2023-05-13 05:45:46 -0600

    Refactoring performed for vegNrgFlux.f90 -- updates to Fortran arithmetic expressions complete -- general clean up about 60 percent complete.

--- build/compile_SUMMA.sh
+++ build/compile_SUMMA.sh
@@ -1,10 +1,14 @@
-## Compile SUMMA -- assumes that this script is in the build directory
+## Compile SUMMA -- assumes that this script is in the build directory -- Designed for Linux
+#How to use: 1) manually set FC, FC_EXE, INCLUDES, and LIBRARIES
+#            2) save file and enter `source compile_SUMMA.sh` in the terminal
 
 ## manually set environment variables
 export FC=gfortran                                                                  # Fortran compiler family
-export FC_EXE=gfortran # name of Fortran executable
+export FC_EXE=gfortran                                                              # name of Fortran executable
 export INCLUDES='-I/usr/include -I/usr/local/include'                               # path to NetCDF and LAPACK include files
 export LIBRARIES='-L/usr/lib -lnetcdff -L/usr/lib/x86_64-linux-gnu -llapack -lblas' # path to libraries
+
+# ------------------------------- Automatic Past This Point ------------------------------------- 
 
 ## automatically set environment variables
 export F_MASTER=$(dirname $PWD)                                                     # parent of build directory
