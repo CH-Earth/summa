800b6c31dc6e0341a8cceeb1df0df9b142a4c934
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-06-22 12:06:31 +0900

    remove debug compile

--- build/cmake/build.mac.bash
+++ build/cmake/build.mac.bash
@@ -2,6 +2,6 @@
   
 # build on Mac, from cmake directory run this as ./build.mac.bash
 
-cmake -B ../cmake_build -S . -DCMAKE_BUILD_TYPE=Sundials_Debug
+cmake -B ../cmake_build -S . -DCMAKE_BUILD_TYPE=Sundials
 cmake --build ../cmake_build --target all
 
