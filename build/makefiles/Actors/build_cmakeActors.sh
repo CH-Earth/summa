27e615592aff1760194e9b573938cf5c3b95cb0a
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-07-27 13:54:27 -0500

    actors build scripts

---
+++ build/makefiles/Actors/build_cmakeActors.sh
@@ -0,0 +1,7 @@
+# from {$maindir}/actor-framework, run 
+# cp ../../summa/build/summa/build/makefiles/Actors/build_cmakeActors.sh build_cmake
+# mkdir install
+# run script from the actor-framework directory with ./build_cmake 
+
+setenv CXX "g++"
+./configure --prefix=../actor-framework/install
