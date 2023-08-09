68378b754bc58d56e63c28556ea67facd1842315
KyleKlenk <kyle.c.klenk@gmail.com>
2023-08-09 08:29:27 -0600

    modified cmake build for working on personal computer

--- build/cmake/build_actors.mac.bash
+++ build/cmake/build_actors.mac.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
   
 # build on Mac, from cmake directory run this as ./build_actors.mac.bash
-
-cmake -B ../cmake_build -S . -DCMAKE_BUILD_TYPE=Sundials_Actors
-cmake --build ../cmake_build --target all
+export FC=gfortran                                            # Fortran compiler family
+export LINK_DIRS='/usr/lib;/usr/lib/x86_64-linux-gnu'                              # Link directories for cmake
+export INCLUDES_DIRS='/usr/include;/usr/local/include'      # directories for INCLUDES cmake variable (cmake uses semicolons as separators)
+export LIBRARY_LINKS='-llapack;-lgfortran;-lnetcdff;-lnetcdf' # list of library links (cmake uses semicolons as separators)
+export DIR_SUNDIALS='/usr/local/sundials-6.3.0'
+cmake -B ../cmake_build_regular -S . -DCMAKE_BUILD_TYPE=Sundials_Actors
+cmake --build ../cmake_build_regular --target all
 
