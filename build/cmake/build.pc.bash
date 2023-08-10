ecd50bae1e68978b3468eba163cc9bf395808cf8
seantrim <strim201325@gmail.com>
2023-08-10 04:36:49 -0600

    Pull in Ashley's Jacobian solver update

--- build/cmake/build.pc.bash
+++ build/cmake/build.pc.bash
@@ -4,11 +4,11 @@
 # Environment variables that must be set for cmake: FC, LINK_DIRS, INCLUDES_DIRS, and LIBRARY_LINKS
 
 # PC Example using Ubuntu
-export FC=gfortran                                            # Fortran compiler family
-export LINK_DIRS=/usr/local/lib                               # Link directories for cmake
-export INCLUDES_DIRS='/usr/include;/usr/local/include'        # directories for INCLUDES cmake variable (cmake uses semicolons as separators)
-export LIBRARY_LINKS='-llapack;-lgfortran;-lnetcdff;-lnetcdf' # list of library links (cmake uses semicolons as separators)
+#export FC=gfortran                                            # Fortran compiler family
+#export LINK_DIRS=/usr/local/lib                               # Link directories for cmake
+#export INCLUDES_DIRS='/usr/include;/usr/local/include'        # directories for INCLUDES cmake variable (cmake uses semicolons as separators)
+#export LIBRARY_LINKS='-llapack;-lgfortran;-lnetcdff;-lnetcdf' # list of library links (cmake uses semicolons as separators)
 
-cmake -B ../cmake_build -S . -DCMAKE_BUILD_TYPE=BE_Debug
+cmake -B ../cmake_build -S . -DCMAKE_BUILD_TYPE=BE
 cmake --build ../cmake_build --target all
 
