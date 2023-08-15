0f2e9df201eb95bb80f8ac5d087d1e25da936dcc
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-08-15 13:31:14 +0900

    Changed so init with canopy liq as small positive (1e-4) if canopy water was small or negative. We were doing this bump on just the water, but then that would get reset as water=liq+ice and water would start at 0, causing the first step to go infeasible in IDA since it was close to infeasible (0 or less). The previous versions of IDA were not properly failing with an infeasible solution, so this was not caught before. Also turned off -flto=1 on mac compilation since was not working well

--- build/cmake/build.mac.bash
+++ build/cmake/build.mac.bash
@@ -8,6 +8,7 @@ export FC=gfortran                                            # Fortran compiler
 export LINK_DIRS=/opt/local/lib                               # Link directories for cmake
 export INCLUDES_DIRS='/opt/local/include;/opt/local/lib'      # directories for INCLUDES cmake variable (cmake uses semicolons as separators)
 export LIBRARY_LINKS='-llapack;-lgfortran;-lnetcdff;-lnetcdf' # list of library links (cmake uses semicolons as separators)
+#export FLAGS_OPT="-flto=1"                                   # -flto=1 is slow to compile, but might want to use
 
 cmake -B ../cmake_build -S . -DCMAKE_BUILD_TYPE=Sundials
 cmake --build ../cmake_build --target all
--- build/cmake/build_ngen.mac.bash
+++ build/cmake/build_ngen.mac.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
   
 # build nextgen on Mac, from ngen directory put this one directory up and run this as ../build_ngen.mac.bash
+# Mac Example using MacPorts:
+export FC=gfortran                                            # Fortran compiler family
+export LINK_DIRS=/opt/local/lib                               # Link directories for cmake
+export INCLUDES_DIRS='/opt/local/include;/opt/local/lib'      # directories for INCLUDES cmake variable (cmake uses semicolons as separators)
+export LIBRARY_LINKS='-llapack;-lgfortran;-lnetcdff;-lnetcdf' # list of library links (cmake uses semicolons as separators)
+#export FLAGS_OPT="-flto=1"                                   # -flto=1 is slow to compile, but might want to use
 
 cmake -B extern/iso_c_fortran_bmi/cmake_build -S extern/iso_c_fortran_bmi
 cmake --build extern/iso_c_fortran_bmi/cmake_build --target all
