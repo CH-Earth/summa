6632772c8e7d2641f9c0cef68db8e890b9eaca18
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-09-06 11:38:52 +0900

    utilites

--- utils/largest_error_attrib.py
+++ utils/largest_error_attrib.py
@@ -33,40 +33,39 @@ attr_fil = Path(attr_fold) / 'attributes.nc'
 # Open the netCDF file with RMSE data
 summa = xr.open_dataset(src_file)
 
-# Get the RMSE variable from the netCDF file
-rmse_var = summa['scalarTotalSoilWat'].sel(stat='rmse')
-#rmse_var = summa['scalarTotalET'].sel(stat='rmse')
-#rmse_var = summa['averageRoutedRunoff'].sel(stat='rmse')
+for setting in settings:
+    # Get the RMSE variable from the netCDF file
+    rmse_var = summa[setting].sel(stat='rmse')
 
-# Mask the finite values of the RMSE variable
-mask = np.isfinite(rmse_var)
-rmse_var = rmse_var[mask]
+    # Mask the finite values of the RMSE variable
+    mask = np.isfinite(rmse_var)
+    rmse_var = rmse_var[mask]
 
-# Get the indices of the largest nBig RMSE values
-max_rmse_indices = rmse_var.argsort()[-nBig:]
+    # Get the indices of the largest nBig RMSE values
+    max_rmse_indices = rmse_var.argsort()[-nBig:]
+    
+    # Get the largest RMSE values
+    rmse_of_max_rmse = rmse_var[max_rmse_indices.values]
+    
+    # Get the hru coordinate of the largest RMSE values
+    hru_of_max_rmse = rmse_var[max_rmse_indices.values].hru.values
+    
+    # Print all the values of the the biggest rmse hru
+    rmse_values = summa.sel(hru=hru_of_max_rmse[nBig-1],stat='rmse')
+    print(setting)
+    print(settings[0:5])
+    print(rmse_values[settings[0]].values,rmse_values[settings[1]].values,rmse_values[settings[2]].values,rmse_values[settings[3]].values,rmse_values[settings[4]].values)
 
-# Get the largest RMSE values
-rmse_of_max_rmse = rmse_var[max_rmse_indices.values]
+    # Open the netCDF file with local attributes
+    attr = xr.open_dataset(attr_fil)
 
-# Get the hru coordinate of the largest RMSE values
-hru_of_max_rmse = rmse_var[max_rmse_indices.values].hru.values
+    # Mask the HRU variable from the netCDF file
+    mask = attr['hruId'].isin(hru_of_max_rmse)
 
-# Print all the values of the the biggest rmse hru
-rmse_values = summa.sel(hru=hru_of_max_rmse[nBig-1],stat='rmse')
-print(settings[0:5])
-print(rmse_values[settings[0]].values,rmse_values[settings[1]].values,rmse_values[settings[2]].values,rmse_values[settings[3]].values,rmse_values[settings[4]].values)
+    # Get the vegTypeIndex variable from the netCDF file
+    veg_type_of_max_rmse = attr['vegTypeIndex'][mask]
 
-# Open the netCDF file with local attributes
-attr = xr.open_dataset(attr_fil)
-
-# Mask the HRU variable from the netCDF file
-mask = attr['hruId'].isin(hru_of_max_rmse)
-
-# Get the vegTypeIndex variable from the netCDF file
-veg_type_of_max_rmse = attr['vegTypeIndex'][mask]
-
-# Print the HRU, vegTypeIndex, and RMSE values of the largest RMSE values
-print("HRU values of the largest RMSE values:", hru_of_max_rmse)
-print("GRU values of the largest RMSE values:", gru_of_max_rmse)
-print("vegTypeIndex values of the largest RMSE values:", veg_type_of_max_rmse.values)
-print("RMSE values of the largest RMSE values:", rmse_of_max_rmse.values)
+    # Print the HRU, vegTypeIndex, and RMSE values of the largest RMSE values
+    print("HRU values of the largest RMSE values:", hru_of_max_rmse)
+    print("vegTypeIndex values of the largest RMSE values:", veg_type_of_max_rmse.values)
+    print("RMSE values of the largest RMSE values:", rmse_of_max_rmse.values)
