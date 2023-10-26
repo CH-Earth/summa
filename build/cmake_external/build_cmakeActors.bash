6893b51ddc1a2b4e29c89f96832a7df5557c5c8f
ashleymedin <ashley.vanbeusekom@gmail.com>
2023-10-26 12:49:45 +0900

    small error on actors build instrucitons

--- build/cmake_external/build_cmakeActors.bash
+++ build/cmake_external/build_cmakeActors.bash
@@ -2,8 +2,8 @@
 
 # from {$maindir}/actor-framework, run
 # cp ../../summa/build/summa/build/cmake_external/build_cmakeActors.bash build_cmake
-# run script from the actor-framework directory with ./build_cmake 
-# run `make`, then `make install`
+# run script from the actor-framework directory with ./build_cmake
+# run `cd build`, `make`, then `make install`
 
 export CXX="g++"
 ./configure --prefix=../install
