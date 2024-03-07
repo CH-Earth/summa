fix balance calculation and utils (should not effect solution)

utils/bal_per_GRU.py
@@ -26,7 +26,7 @@ testing = False
 if testing: 
     stat = 'mean'
     viz_dir = Path('/Users/amedin/Research/USask/test_py/statistics')
-    method_name=['be1cm','be1en','be1lu'] #maybe make this an argument
+    method_name=['be1'] #cm','be1en','be1lu'] #maybe make this an argument
 else:
     import sys
     # The first input argument specifies the run where the files are
@@ -74,8 +74,9 @@ def run_loop(i,var,comp,leg_t,leg_t0,plt_t):
 
     # Data
     for m in method_name:
-        s = np.fabs(summa[m][var].sel(stat=stat))
-        s0 = np.fabs(summa[m][comp].sel(stat=stat))
+        # Get the statistics, remove 9999 (should be nan, but just in case)
+        s = np.fabs(summa[m][var].sel(stat=stat)).where(lambda x: x != 9999)
+        s0 = np.fabs(summa[m][comp].sel(stat=stat)).where(lambda x: x != 9999)
 
         if var == 'wallClockTime':
             stat0_word = 'Number flux calculations'

utils/plot_per_GRUMultBal.py
@@ -181,9 +181,9 @@ for plot_var in plot_vars:
         # Make absolute value norm, not all positive
         s = np.fabs(s) 
 
-        # Replace inf values with NaN in the s DataArray
-        s = s.where(~np.isinf(s), np.nan)
-
+        # Replace inf and 9999 values with NaN in the s DataArray
+        s = s.where(~np.isinf(s), np.nan).where(lambda x: x != 9999, np.nan)
+        
         hru_ids = [hru_id for hru_id in hru_ids_shp.values if hru_id in s.hru.values] #if some missing
         bas_albers[plot_var+m] = s.sel(hru=hru_ids)
         #bas_albers[plot_var+m] = s.sel(hru=hru_ids_shp.values)